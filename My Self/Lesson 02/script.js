c0e75f0088b934e07e73cf711e9a2645b0c1d84b
Ruchikaupuldeniya <ruchikaupuldeniya14@gmail.com>
2026-07-14 18:48:31 +0530

    Add files via upload

---
+++ My Self/Lesson 02/script.js
@@ -0,0 +1,33 @@
+
+// comment in js
+
+// this is an one line comment
+
+/* this is a multi line comment
+line 02
+line 03
+like this
+*/
+
+
+//x = 1; // we assign the value 1 to the x
+
+// data type in js
+
+/*
+
+boolean
+string
+int 
+
+*/
+
+var x = 3;
+console.log(x)
+x = 5;
+
+x = x +1 ;
+console.log(x)
+
+x ++ ;
+console.log(x)
